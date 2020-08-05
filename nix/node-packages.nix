0b49e16f5af2ae5886478949808d14c688c5a8ba
Robert Cronin <robert.cronin@gatech.edu>
2020-08-05 17:59:31 +0800

    v0.0.14

--- nix/node-packages.nix
+++ nix/node-packages.nix
@@ -3554,7 +3554,7 @@ let
   args = {
     name = "js-polykey";
     packageName = "js-polykey";
-    version = "0.0.13";
+    version = "0.0.14";
     src = ./..;
     dependencies = [
       sources."@babel/code-frame-7.0.0"
