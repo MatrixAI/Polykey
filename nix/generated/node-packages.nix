9f06e8386ab308237ecc0e651a66d0cffa3f682e
Robert Cronin <robert.cronin@gatech.edu>
2020-10-15 17:37:14 +0800

    v0.0.20

--- nix/generated/node-packages.nix
+++ nix/generated/node-packages.nix
@@ -13337,7 +13337,7 @@ let
   args = {
     name = "_at_matrixai_slash_polykey";
     packageName = "@matrixai/polykey";
-    version = "0.0.19";
+    version = "0.0.20";
     src = ../..;
     dependencies = [
       sources."@apidevtools/json-schema-ref-parser-9.0.6"
