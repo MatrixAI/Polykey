204b09a1d851b4478408fd18b3c846dbc368c339
Roger Qiu <roger.qiu@polyhack.io>
2021-03-29 12:55:52 +1100

    The index.test.ts should not start a side effect

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,13 +2,11 @@ import Logger, { LogLevel, StreamHandler } from '@matrixai/logger';
 import Polykey from '@';
 
 describe('index', () => {
-  const logger = new Logger('Polykey Test', LogLevel.WARN, [
+  const logger = new Logger('index test', LogLevel.WARN, [
     new StreamHandler(),
   ]);
   test('construction of Polykey', async () => {
     const pk = new Polykey({ logger });
-    await pk.start({ password: 'password' });
     expect(pk).toBeInstanceOf(Polykey);
-    await pk.stop();
   });
 });
