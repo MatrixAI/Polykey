c7715737e8b245ad2a8b1b4a962b7043170a5985
Roger Qiu <roger.qiu@polyhack.io>
2021-03-29 13:38:50 +1100

    Linting

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,9 +2,7 @@ import Logger, { LogLevel, StreamHandler } from '@matrixai/logger';
 import Polykey from '@';
 
 describe('index', () => {
-  const logger = new Logger('index test', LogLevel.WARN, [
-    new StreamHandler(),
-  ]);
+  const logger = new Logger('index test', LogLevel.WARN, [new StreamHandler()]);
   test('construction of Polykey', async () => {
     const pk = new Polykey({ logger });
     expect(pk).toBeInstanceOf(Polykey);
