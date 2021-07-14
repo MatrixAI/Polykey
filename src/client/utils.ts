a1668cbee2f4d00061f2e42ef99d6d1bd25817bc
Scott <scott.morris@matrixai.com>
2021-07-14 18:37:53 +1000

    wip

--- src/client/utils.ts
+++ src/client/utils.ts
@@ -1,9 +1,15 @@
 import fs from 'fs';
+import path from 'path';
+import globrex from 'globrex';
+import globalyzer from 'globalyzer';
 import * as grpc from '@grpc/grpc-js';
 
 import * as clientErrors from './errors';
 import { SessionManager } from '../session';
 import { VaultManager } from '../vaults';
+
+const isHidden = /(^|[\\\/])\.[^\\\/\.]/g;
+let CACHE = {};
 
 async function checkPassword(
   meta: grpc.Metadata,
@@ -28,4 +34,67 @@ function parseVaultInput(input: string, vaultManager: VaultManager): string {
   }
 }
 
-export { checkPassword, parseVaultInput };
+async function walk(output, prefix, lexer, opts, dirname='', level=0) {
+  const rgx = lexer.segments[level];
+  const dir = path.resolve(opts.cwd, prefix, dirname);
+  const files = await readdir(dir);
+  const { dot, filesOnly } = opts;
+
+  let i=0, len=files.length, file;
+  let fullpath, relpath, stats, isMatch;
+
+  for (; i < len; i++) {
+    fullpath = path.join(dir, file=files[i]);
+    relpath = dirname ? path.join(dirname, file) : file;
+    if (!dot && isHidden.test(relpath)) continue;
+    isMatch = lexer.regex.test(relpath);
+
+    if ((stats=CACHE[relpath]) === void 0) {
+      CACHE[relpath] = stats = await lstat(fullpath);
+    }
+
+    if (!stats.isDirectory()) {
+      isMatch && output.push(path.relative(opts.cwd, fullpath));
+      continue;
+    }
+
+    if (rgx && !rgx.test(file)) continue;
+    !filesOnly && isMatch && output.push(path.join(prefix, relpath));
+
+    await walk(output, prefix, lexer, opts, relpath, rgx && rgx.toString() !== lexer.globstar && level + 1);
+  }
+}
+
+async function glob(str: string, opts={ cwd: '.', absolute: true, filesOnly: false, flush: true }) {
+  if (!str) return [];
+
+  let glob = globalyzer(str);
+
+  opts.cwd = opts.cwd || '.';
+
+  if (!glob.isGlob) {
+    try {
+      let resolved = path.resolve(opts.cwd, str);
+      let dirent = await stat(resolved);
+      if (opts.filesOnly && !dirent.isFile()) return [];
+
+      return opts.absolute ? [resolved] : [str];
+    } catch (err) {
+      if (err.code != 'ENOENT') throw err;
+
+      return [];
+    }
+  }
+
+  if (opts.flush) CACHE = {};
+
+  let matches = [];
+  const res = globrex(glob.glob, { filepath:true, globstar:true, extended:true });
+  const globPath = res.path;
+
+  await walk(matches, glob.base, globPath, opts, '.', 0);
+
+  return opts.absolute ? matches.map(x => path.resolve(opts.cwd, x)) : matches;
+}
+
+export { checkPassword, parseVaultInput, glob };
