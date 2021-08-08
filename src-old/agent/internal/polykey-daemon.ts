c41d89d2e34a6c6bb48ce9bf32a4269038d63154
Roger Qiu <roger.qiu@polyhack.io>
2021-08-08 22:07:54 +1000

    Removing old source code: proto, schemas, nodes, discovery, certs, bin, agent

--- src-old/agent/internal/polykey-daemon.ts
+++
@@ -1,9 +0,0 @@
-import { PolykeyAgent } from '../../Polykey';
-
-let agent: PolykeyAgent;
-
-process.on('message', async (polykeyPath: string) => {
-  agent = new PolykeyAgent(polykeyPath);
-  await agent.startServer();
-  process.send && process.send('started');
-});
