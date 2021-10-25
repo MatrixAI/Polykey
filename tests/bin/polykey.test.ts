fb3826d81e8bde01f66fa58c0f7c038a575c1e50
Roger Qiu <roger.qiu@polyhack.io>
2021-10-25 22:42:21 +1100

    Fixed up tests/bin/polykey.test.ts to test the contents of STDOUT and STDERR

--- tests/bin/polykey.test.ts
+++ tests/bin/polykey.test.ts
@@ -4,36 +4,7 @@ describe('polykey', () => {
   test('default help display', async () => {
     const result = await pkWithStdio([]);
     expect(result.code).toBe(0);
-    console.log(result.stdout)
-    console.log(result.stderr)
+    expect(result.stdout).toBe("");
+    expect(result.stderr.length > 0).toBe(true);
   });
 });
-
-// Describe('CLI echoes', () => {
-//   beforeEach(async () => {
-//     dataDir = await fs.promises.mkdtemp(
-//       path.join(os.tmpdir(), 'polykey-test-'),
-//     );
-//     polykeyAgent = await PolykeyAgent.createPolykey({
-//       nodePath: dataDir,
-//       logger: logger,
-//     });
-//     await polykeyAgent.start({
-//       password: 'password',
-//     });
-//   });
-
-//   afterEach(async () => {
-//     await polykeyAgent.stop();
-//   });
-
-//   test('should echo', async () => {
-//     const result = await pk(['echoes', 'echo', '-np', dataDir, 'HelloWorld']);
-//     expect(result).toBe(0);
-//   });
-
-//   test('should cause error', async () => {
-//     const result = await pk(['echoes', 'echo', '-np', dataDir, 'ThrowAnError']);
-//     expect(result).toBe(1);
-//   });
-// });
