8e234fa7af5301ff32218c4edd704b1183d97258
Roger Qiu <roger.qiu@polyhack.io>
2021-10-25 23:30:49 +1100

    Changing "" to  for tests/bin/polykey.test.ts

--- tests/bin/polykey.test.ts
+++ tests/bin/polykey.test.ts
@@ -4,7 +4,7 @@ describe('polykey', () => {
   test('default help display', async () => {
     const result = await pkWithStdio([]);
     expect(result.code).toBe(0);
-    expect(result.stdout).toBe("");
+    expect(result.stdout).toBe('');
     expect(result.stderr.length > 0).toBe(true);
   });
 });
