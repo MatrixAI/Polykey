2528624e56f269cf5be267ddb439f29a93811127
Roger Qiu <roger.qiu@polyhack.io>
2021-11-25 00:32:33 +1100

    Fixing js-async-init integration into Discovery

--- tests/discovery/Discovery.test.ts
+++ tests/discovery/Discovery.test.ts
@@ -1,45 +1,44 @@
-import Logger, { LogLevel } from '@matrixai/logger';
+import type { ClaimLinkIdentity, ClaimLinkNode } from '@/claims/types';
+import type { IdentityId } from '@/identities/types';
 import fs from 'fs';
 import path from 'path';
 import os from 'os';
+import Logger, { LogLevel } from '@matrixai/logger';
 import { Discovery } from '@/discovery';
 import PolykeyAgent from '@/PolykeyAgent';
+import * as discoveryErrors from '@/discovery/errors';
+import TestProvider from '../identities/TestProvider';
 import {
   addRemoteDetails,
   cleanupRemoteKeynode,
   setupRemoteKeynode,
 } from '../utils';
-import { ClaimLinkIdentity, ClaimLinkNode } from '@/claims/types';
-import TestProvider from '../identities/TestProvider';
-import { IdentityId } from '@/identities/types';
 
 describe('Discovery', () => {
-  //Constants.
+  // Constants.
   const password = 'password';
   const logger = new Logger('Discovery Tests', LogLevel.WARN);
 
   describe('Basic manager tests.', () => {
-    //Managers
+    // Managers
     let polykeyAgent: PolykeyAgent;
     let discovery: Discovery;
 
-    //Directories
+    // Directories
     let dataDirEach: string;
     let nodePath: string;
 
     beforeAll(async () => {
-      //Creating directories and paths.
+      // Creating directories and paths.
       dataDirEach = await fs.promises.mkdtemp(
         path.join(os.tmpdir(), 'polykey-test-'),
       );
       nodePath = path.join(dataDirEach, 'node');
 
-      polykeyAgent = await PolykeyAgent.createPolykey({
+      polykeyAgent = await PolykeyAgent.createPolykeyAgent({
         password,
         nodePath,
         logger,
-        cores: 1,
-        workerManager: null,
       });
       discovery = polykeyAgent.discovery;
     });
@@ -58,12 +57,10 @@ describe('Discovery', () => {
     test(
       'Starts and stops',
       async () => {
-        await polykeyAgent.start({});
-
-        //Not started.
+        // Not started.
         expect(discovery.destroyed).toBeFalsy();
 
-        //Starting.
+        // Starting.
         await discovery.destroy();
         expect(discovery.destroyed).toBeTruthy();
       },
@@ -84,23 +81,23 @@ describe('Discovery', () => {
       nodeB = await setupRemoteKeynode({ logger });
       nodeC = await setupRemoteKeynode({ logger });
 
-      //Forming links
-      //A->B->C
-      //Adding connection details.
+      // Forming links
+      // A->B->C
+      // Adding connection details.
       await addRemoteDetails(nodeA, nodeB);
       await addRemoteDetails(nodeB, nodeA);
       await addRemoteDetails(nodeB, nodeC);
       await addRemoteDetails(nodeC, nodeB);
-      //Adding sigchain details.
+      // Adding sigchain details.
       const claimBtoC: ClaimLinkNode = {
         type: 'node',
-        node1: nodeB.nodes.getNodeId(),
-        node2: nodeC.nodes.getNodeId(),
+        node1: nodeB.nodeManager.getNodeId(),
+        node2: nodeC.nodeManager.getNodeId(),
       };
       const claimCtoB: ClaimLinkNode = {
         type: 'node',
-        node1: nodeC.nodes.getNodeId(),
-        node2: nodeB.nodes.getNodeId(),
+        node1: nodeC.nodeManager.getNodeId(),
+        node2: nodeB.nodeManager.getNodeId(),
       };
       await nodeB.sigchain.addClaim(claimBtoC);
       await nodeB.sigchain.addClaim(claimCtoB);
@@ -108,16 +105,16 @@ describe('Discovery', () => {
       await nodeC.sigchain.addClaim(claimBtoC);
 
       testProvider = new TestProvider();
-      nodeA.identities.registerProvider(testProvider);
+      nodeA.identitiesManager.registerProvider(testProvider);
 
-      //Setting up identtiy.
+      // Setting up identtiy.
       const gen = await testProvider.authenticate();
       await gen.next();
       identityId = (await gen.next()).value as IdentityId;
 
       const claimIdentToB: ClaimLinkIdentity = {
         type: 'identity',
-        node: nodeB.nodes.getNodeId(),
+        node: nodeB.nodeManager.getNodeId(),
         provider: testProvider.id,
         identity: identityId,
       };
@@ -129,31 +126,31 @@ describe('Discovery', () => {
       await cleanupRemoteKeynode(nodeC);
     });
     beforeEach(async () => {
-      await nodeA.gestalts.clearDB();
-      await nodeB.gestalts.clearDB();
-      await nodeC.gestalts.clearDB();
+      await nodeA.gestaltGraph.clearDB();
+      await nodeB.gestaltGraph.clearDB();
+      await nodeC.gestaltGraph.clearDB();
     });
     test('discoverGestaltByNode by NodeId', async () => {
-      //Time to do the test.
+      // Time to do the test.
       const discoverProcess = nodeA.discovery.discoverGestaltByNode(
-        nodeB.nodes.getNodeId(),
+        nodeB.nodeManager.getNodeId(),
       );
       for await (const _step of discoverProcess) {
         // Waiting for the discovery process to finish.
       }
 
-      //We expect to find a gestalt now.
-      const gestalt = await nodeA.gestalts.getGestalts();
+      // We expect to find a gestalt now.
+      const gestalt = await nodeA.gestaltGraph.getGestalts();
       expect(gestalt.length).not.toBe(0);
       const gestaltString = JSON.stringify(gestalt);
-      expect(gestaltString).toContain(nodeB.nodes.getNodeId());
-      expect(gestaltString).toContain(nodeC.nodes.getNodeId());
+      expect(gestaltString).toContain(nodeB.nodeManager.getNodeId());
+      expect(gestaltString).toContain(nodeC.nodeManager.getNodeId());
       expect(gestaltString).toContain(identityId);
     });
     test('discoverGestaltByNode by IdentityId', async () => {
       await testProvider.overrideLinks(
-        nodeB.nodes.getNodeId(),
-        nodeB.keys.getRootKeyPairPem().privateKey,
+        nodeB.nodeManager.getNodeId(),
+        nodeB.keyManager.getRootKeyPairPem().privateKey,
       );
       // Time to do the test.
       const discoverProcess = nodeA.discovery.discoverGestaltByIdentity(
@@ -164,13 +161,22 @@ describe('Discovery', () => {
         // Waiting for the discovery process to finish.
       }
 
-      //We expect to find a gestalt now.
-      const gestalt = await nodeA.gestalts.getGestalts();
+      // We expect to find a gestalt now.
+      const gestalt = await nodeA.gestaltGraph.getGestalts();
       expect(gestalt.length).not.toBe(0);
       const gestaltString = JSON.stringify(gestalt);
-      expect(gestaltString).toContain(nodeB.nodes.getNodeId());
-      expect(gestaltString).toContain(nodeC.nodes.getNodeId());
+      expect(gestaltString).toContain(nodeB.nodeManager.getNodeId());
+      expect(gestaltString).toContain(nodeC.nodeManager.getNodeId());
       expect(gestaltString).toContain(identityId);
+    });
+    test('discovery readiness', async () => {
+      await nodeA.discovery.destroy();
+      await expect(async () =>
+        nodeA.discovery.discoverGestaltByNode(nodeB.nodeManager.getNodeId()),
+      ).rejects.toThrow(discoveryErrors.ErrorDiscoveryDestroyed);
+      await expect(async () =>
+        nodeA.discovery.discoverGestaltByIdentity(testProvider.id, identityId),
+      ).rejects.toThrow(discoveryErrors.ErrorDiscoveryDestroyed);
     });
   });
   describe('End-to-end discovery between two gestalts', () => {
@@ -191,7 +197,7 @@ describe('Discovery', () => {
       nodeC = await setupRemoteKeynode({ logger });
       nodeD = await setupRemoteKeynode({ logger });
 
-      //Adding connection details
+      // Adding connection details
       await addRemoteDetails(nodeA, nodeB);
       await addRemoteDetails(nodeA, nodeD);
       await addRemoteDetails(nodeB, nodeA);
@@ -201,12 +207,12 @@ describe('Discovery', () => {
 
       // Setting up identity provider
       testProvider = new TestProvider();
-      nodeA.identities.registerProvider(testProvider);
-      nodeC.identities.registerProvider(testProvider);
+      nodeA.identitiesManager.registerProvider(testProvider);
+      nodeC.identitiesManager.registerProvider(testProvider);
 
       // Forming gestalts.
-      await nodeA.nodes.claimNode(nodeB.nodes.getNodeId());
-      await nodeC.nodes.claimNode(nodeD.nodes.getNodeId());
+      await nodeA.nodeManager.claimNode(nodeB.nodeManager.getNodeId());
+      await nodeC.nodeManager.claimNode(nodeD.nodeManager.getNodeId());
 
       const gen1 = testProvider.authenticate();
       await gen1.next();
@@ -217,7 +223,7 @@ describe('Discovery', () => {
 
       const claimIdentToB: ClaimLinkIdentity = {
         type: 'identity',
-        node: nodeB.nodes.getNodeId(),
+        node: nodeB.nodeManager.getNodeId(),
         provider: testProvider.id,
         identity: identityIdA,
       };
@@ -225,7 +231,7 @@ describe('Discovery', () => {
 
       const claimIdentToD: ClaimLinkIdentity = {
         type: 'identity',
-        node: nodeD.nodes.getNodeId(),
+        node: nodeD.nodeManager.getNodeId(),
         provider: testProvider.id,
         identity: identityIdB,
       };
@@ -238,15 +244,15 @@ describe('Discovery', () => {
       await cleanupRemoteKeynode(nodeD);
     });
     beforeEach(async () => {
-      await nodeA.gestalts.clearDB();
-      await nodeB.gestalts.clearDB();
-      await nodeC.gestalts.clearDB();
-      await nodeD.gestalts.clearDB();
+      await nodeA.gestaltGraph.clearDB();
+      await nodeB.gestaltGraph.clearDB();
+      await nodeC.gestaltGraph.clearDB();
+      await nodeD.gestaltGraph.clearDB();
     });
     test('Gestalt1 discovers Gestalt2', async () => {
       await testProvider.overrideLinks(
-        nodeD.nodes.getNodeId(),
-        nodeD.keys.getRootKeyPairPem().privateKey,
+        nodeD.nodeManager.getNodeId(),
+        nodeD.keyManager.getRootKeyPairPem().privateKey,
       );
       // Time to do the test.
       const discoverProcess = nodeA.discovery.discoverGestaltByIdentity(
@@ -257,18 +263,18 @@ describe('Discovery', () => {
         // Waiting for the discovery process to finish.
       }
 
-      //We expect to find a gestalt now.
-      const gestalt = await nodeA.gestalts.getGestalts();
+      // We expect to find a gestalt now.
+      const gestalt = await nodeA.gestaltGraph.getGestalts();
       expect(gestalt.length).not.toBe(0);
       const gestaltString = JSON.stringify(gestalt);
-      expect(gestaltString).toContain(nodeC.nodes.getNodeId());
-      expect(gestaltString).toContain(nodeD.nodes.getNodeId());
+      expect(gestaltString).toContain(nodeC.nodeManager.getNodeId());
+      expect(gestaltString).toContain(nodeD.nodeManager.getNodeId());
       expect(gestaltString).toContain(identityIdB);
     });
     test('Gestalt2 discovers Gestalt1', async () => {
       await testProvider.overrideLinks(
-        nodeB.nodes.getNodeId(),
-        nodeB.keys.getRootKeyPairPem().privateKey,
+        nodeB.nodeManager.getNodeId(),
+        nodeB.keyManager.getRootKeyPairPem().privateKey,
       );
       // Time to do the test.
       const discoverProcess = nodeC.discovery.discoverGestaltByIdentity(
@@ -279,12 +285,12 @@ describe('Discovery', () => {
         // Waiting for the discovery process to finish.
       }
 
-      //We expect to find a gestalt now.
-      const gestalt = await nodeC.gestalts.getGestalts();
+      // We expect to find a gestalt now.
+      const gestalt = await nodeC.gestaltGraph.getGestalts();
       expect(gestalt.length).not.toBe(0);
       const gestaltString = JSON.stringify(gestalt);
-      expect(gestaltString).toContain(nodeA.nodes.getNodeId());
-      expect(gestaltString).toContain(nodeB.nodes.getNodeId());
+      expect(gestaltString).toContain(nodeA.nodeManager.getNodeId());
+      expect(gestaltString).toContain(nodeB.nodeManager.getNodeId());
       expect(gestaltString).toContain(identityIdA);
     });
   });
