42454b9885e5fb1c69c1cd028dcceedf817854b6
Emma Casolin <ecasolin@hotmail.com>
2021-12-13 13:29:07 +1100

    test fixes from status command implementation

--- tests/client/GRPCClientClient.test.ts
+++ tests/client/GRPCClientClient.test.ts
@@ -5,12 +5,13 @@ import os from 'os';
 import path from 'path';
 import fs from 'fs';
 import Logger, { LogLevel, StreamHandler } from '@matrixai/logger';
-
 import { GRPCClientClient } from '@/client';
 import * as utilsPB from '@/proto/js/polykey/v1/utils/utils_pb';
 import { PolykeyAgent } from '@';
+import { Status } from '@/status';
 import * as binProcessors from '@/bin/utils/processors';
 import { Session } from '@/sessions';
+import config from '@/config';
 import { errors as clientErrors } from '@/client';
 import * as testUtils from './utils';
 
@@ -29,14 +30,12 @@ describe('GRPCClientClient', () => {
   let client: GRPCClientClient;
   let server: grpc.Server;
   let port: number;
-
   let polykeyAgent: PolykeyAgent;
   let dataDir: string;
   let nodePath: string;
-
   let nodeId: NodeId;
-
-  beforeEach(async () => {
+  let session: Session;
+  beforeAll(async () => {
     dataDir = await fs.promises.mkdtemp(
       path.join(os.tmpdir(), 'polykey-test-'),
     );
@@ -46,7 +45,6 @@ describe('GRPCClientClient', () => {
       nodePath,
       logger: logger,
     });
-
     nodeId = polykeyAgent.nodeManager.getNodeId();
     [server, port] = await testUtils.openTestClientServer({
       polykeyAgent,
@@ -57,6 +55,18 @@ describe('GRPCClientClient', () => {
     await session.start({
       sessionToken,
     });
+  }, global.polykeyStartupTimeout);
+  afterAll(async () => {
+    await client.destroy();
+    await testUtils.closeTestClientServer(server);
+    await polykeyAgent.stop();
+    await polykeyAgent.destroy();
+    await fs.promises.rm(dataDir, {
+      force: true,
+      recursive: true,
+    });
+  });
+  test('cannot be called when destroyed', async () => {
     client = await GRPCClientClient.createGRPCClientClient({
       nodeId: nodeId,
       host: '127.0.0.1' as Host,
@@ -66,35 +76,49 @@ describe('GRPCClientClient', () => {
       timeout: 10000,
       session: session,
     });
-  }, global.polykeyStartupTimeout * 3);
-  afterEach(async () => {
-    await client.destroy();
-    await testUtils.closeTestClientServer(server);
-
-    await polykeyAgent.stop();
-    await polykeyAgent.destroy();
-
-    await fs.promises.rm(dataDir, {
-      force: true,
-      recursive: true,
-    });
-  });
-
-  test('cannot be called when destroyed', async () => {
     await client.destroy();
     await expect(async () => {
       await client.agentStatus(new utilsPB.EmptyMessage());
     }).rejects.toThrow(clientErrors.ErrorClientClientDestroyed);
   });
   test('can get status', async () => {
+    client = await GRPCClientClient.createGRPCClientClient({
+      nodeId: nodeId,
+      host: '127.0.0.1' as Host,
+      port: port as Port,
+      tlsConfig: { keyPrivatePem: undefined, certChainPem: undefined },
+      logger: logger,
+      timeout: 10000,
+      session: session,
+    });
     await fs.promises.writeFile(path.join(dataDir, 'password'), password);
     const meta = await binProcessors.processAuthentication(
       path.join(dataDir, 'password'),
       fs,
     );
+    const status = new Status({
+      statusPath: path.join(nodePath, config.defaults.statusBase),
+      fs,
+      logger,
+    });
+    const statusInfo = (await status.readStatus())!;
     const emptyMessage = new utilsPB.EmptyMessage();
     const response = await client.agentStatus(emptyMessage, meta);
-    expect(response.getAddress()).toBeTruthy();
+    expect(typeof response.getPid()).toBe('number');
+    expect(response.getNodeId()).toBe(statusInfo.data.nodeId);
+    expect(response.getClientHost()).toBe(statusInfo.data.clientHost);
+    expect(response.getClientPort()).toBe(statusInfo.data.clientPort);
+    expect(response.getIngressHost()).toBe(statusInfo.data.ingressHost);
+    expect(response.getIngressPort()).toBe(statusInfo.data.ingressPort);
+    expect(typeof response.getEgressHost()).toBe('string');
+    expect(typeof response.getEgressPort()).toBe('number');
+    expect(typeof response.getAgentHost()).toBe('string');
+    expect(typeof response.getAgentPort()).toBe('number');
+    expect(typeof response.getProxyHost()).toBe('string');
+    expect(typeof response.getProxyPort()).toBe('number');
+    expect(typeof response.getRootPublicKeyPem()).toBe('string');
+    expect(typeof response.getRootCertPem()).toBe('string');
+    expect(typeof response.getRootCertChainPem()).toBe('string');
     await client.destroy();
   });
 });
--- tests/client/PolykeyClient.test.ts
+++ tests/client/PolykeyClient.test.ts
@@ -1,15 +1,16 @@
 import type * as grpc from '@grpc/grpc-js';
 import type { GRPCClientClient } from '@/client';
+import type { SessionToken } from '@/sessions/types';
 import os from 'os';
 import path from 'path';
 import fs from 'fs';
 import Logger, { LogLevel, StreamHandler } from '@matrixai/logger';
 import * as binProcessors from '@/bin/utils/processors';
-
 import { PolykeyClient } from '@';
+import { Status } from '@/status';
 import * as utilsPB from '@/proto/js/polykey/v1/utils/utils_pb';
 import { PolykeyAgent } from '@';
-
+import config from '@/config';
 import * as testUtils from './utils';
 
 // Mocks.
@@ -30,34 +31,39 @@ describe('PolykeyClient', () => {
   let _port: number;
   let passwordFile: string;
   let meta: grpc.Metadata;
-
   let dataDir: string;
   let nodePath: string;
+  let nodePath2: string;
   let clientPath: string;
-
+  let clientPath2: string;
   let polykeyAgent: PolykeyAgent;
-
-  beforeEach(async () => {
+  let polykeyAgent2: PolykeyAgent;
+  let sessionToken: SessionToken;
+  beforeAll(async () => {
     dataDir = await fs.promises.mkdtemp(
       path.join(os.tmpdir(), 'polykey-test-'),
     );
-    nodePath = path.join(dataDir, 'node');
-    clientPath = path.join(dataDir, 'client');
+    nodePath = path.join(dataDir, 'keynode1');
+    clientPath = path.join(dataDir, 'client1');
+    nodePath2 = path.join(dataDir, 'keynode2');
+    clientPath2 = path.join(dataDir, 'client2');
     passwordFile = path.join(dataDir, 'password');
     await fs.promises.writeFile(passwordFile, password);
     meta = await binProcessors.processAuthentication(passwordFile, fs);
-
     polykeyAgent = await PolykeyAgent.createPolykeyAgent({
       password,
       nodePath,
       logger: logger,
     });
-
+    polykeyAgent2 = await PolykeyAgent.createPolykeyAgent({
+      password,
+      nodePath: nodePath2,
+      logger: logger.getChild(PolykeyAgent.name),
+    });
     [server, _port] = await testUtils.openTestClientServer({
       polykeyAgent,
       secure: false,
     });
-
     pkClient = await PolykeyClient.createPolykeyClient({
       nodeId: polykeyAgent.keyManager.getNodeId(),
       host: polykeyAgent.grpcServerClient.host,
@@ -67,84 +73,84 @@ describe('PolykeyClient', () => {
       logger: logger,
     });
     client = pkClient.grpcClient;
-
-    const sessionToken = await polykeyAgent.sessionManager.createToken();
+    sessionToken = await polykeyAgent.sessionManager.createToken();
     await pkClient.session.start({ sessionToken });
   });
-  afterEach(async () => {
+  afterAll(async () => {
     await client.destroy();
     await pkClient.stop();
     await testUtils.closeTestClientServer(server);
-
+    await polykeyAgent2.stop();
+    await polykeyAgent2.destroy();
     await polykeyAgent.stop();
     await polykeyAgent.destroy();
-
     await fs.promises.rm(dataDir, {
       force: true,
       recursive: true,
     });
   });
   test('can get status', async () => {
+    const status = new Status({
+      statusPath: path.join(nodePath, config.defaults.statusBase),
+      fs,
+      logger,
+    });
+    const statusInfo = (await status.readStatus())!;
     const emptyMessage = new utilsPB.EmptyMessage();
     const response = await client.agentStatus(emptyMessage, meta);
-    expect(response.getNodeId()).toBeTruthy();
-    expect(response.getAddress()).toBeTruthy();
-    expect(response.getCert()).toBeTruthy();
+    expect(typeof response.getPid()).toBe('number');
+    expect(response.getNodeId()).toBe(statusInfo.data.nodeId);
+    expect(response.getClientHost()).toBe(statusInfo.data.clientHost);
+    expect(response.getClientPort()).toBe(statusInfo.data.clientPort);
+    expect(response.getIngressHost()).toBe(statusInfo.data.ingressHost);
+    expect(response.getIngressPort()).toBe(statusInfo.data.ingressPort);
+    expect(typeof response.getEgressHost()).toBe('string');
+    expect(typeof response.getEgressPort()).toBe('number');
+    expect(typeof response.getAgentHost()).toBe('string');
+    expect(typeof response.getAgentPort()).toBe('number');
+    expect(typeof response.getProxyHost()).toBe('string');
+    expect(typeof response.getProxyPort()).toBe('number');
+    expect(typeof response.getRootPublicKeyPem()).toBe('string');
+    expect(typeof response.getRootCertPem()).toBe('string');
+    expect(typeof response.getRootCertChainPem()).toBe('string');
   });
-  describe('TLS tests', () => {
-    const password = 'password';
-    const logger = new Logger('PolykeyAgent TLS', LogLevel.WARN, [
-      new StreamHandler(),
-    ]);
-    let dataDir: string;
-    let nodePath2: string;
-    let clientPath2: string;
-    let polykeyAgent2: PolykeyAgent;
-    let sessionToken;
-
-    beforeAll(async () => {
-      // Setting up paths and dirs.
-      dataDir = await fs.promises.mkdtemp(
-        path.join(os.tmpdir(), 'polykey-test-'),
-      );
-      nodePath2 = path.join(dataDir, 'keynode');
-      clientPath2 = path.join(dataDir, 'client2');
-
-      // Starting an agent.
-      polykeyAgent2 = await PolykeyAgent.createPolykeyAgent({
-        password,
-        nodePath: nodePath2,
-        logger: logger.getChild(PolykeyAgent.name),
-      });
-
-      sessionToken = await polykeyAgent2.sessionManager.createToken();
-    }, global.defaultTimeout * 3);
-    afterAll(async () => {
-      await polykeyAgent2.stop();
-      await polykeyAgent2.destroy();
+  test('can get status over TLS', async () => {
+    // Starting client.
+    const pkClient = await PolykeyClient.createPolykeyClient({
+      nodeId: polykeyAgent2.keyManager.getNodeId(),
+      host: polykeyAgent2.grpcServerClient.host,
+      port: polykeyAgent2.grpcServerClient.port,
+      nodePath: clientPath2,
+      fs: fs,
+      logger: logger.getChild(PolykeyClient.name),
     });
-    test('can get status over TLS', async () => {
-      // Starting client.
-      const pkClient = await PolykeyClient.createPolykeyClient({
-        nodeId: polykeyAgent2.keyManager.getNodeId(),
-        host: polykeyAgent2.grpcServerClient.host,
-        port: polykeyAgent2.grpcServerClient.port,
-        nodePath: clientPath2,
-        fs: fs,
-        logger: logger.getChild(PolykeyClient.name),
-      });
-      await pkClient.session.start({ sessionToken });
-      const meta = await binProcessors.processAuthentication(passwordFile, fs);
+    await pkClient.session.start({ sessionToken });
+    const meta = await binProcessors.processAuthentication(passwordFile, fs);
 
-      const emptyMessage = new utilsPB.EmptyMessage();
-      const response = await pkClient.grpcClient.agentStatus(
-        emptyMessage,
-        meta,
-      );
-      expect(response.getNodeId()).toBeTruthy();
-      expect(response.getAddress()).toBeTruthy();
-      expect(response.getCert()).toBeTruthy();
-      expect(pkClient.grpcClient.secured).toBeTruthy();
+    const status = new Status({
+      statusPath: path.join(nodePath2, config.defaults.statusBase),
+      fs,
+      logger,
     });
+    const statusInfo = (await status.readStatus())!;
+
+    const emptyMessage = new utilsPB.EmptyMessage();
+    const response = await pkClient.grpcClient.agentStatus(emptyMessage, meta);
+    expect(typeof response.getPid()).toBe('number');
+    expect(response.getNodeId()).toBe(statusInfo.data.nodeId);
+    expect(response.getClientHost()).toBe(statusInfo.data.clientHost);
+    expect(response.getClientPort()).toBe(statusInfo.data.clientPort);
+    expect(response.getIngressHost()).toBe(statusInfo.data.ingressHost);
+    expect(response.getIngressPort()).toBe(statusInfo.data.ingressPort);
+    expect(typeof response.getEgressHost()).toBe('string');
+    expect(typeof response.getEgressPort()).toBe('number');
+    expect(typeof response.getAgentHost()).toBe('string');
+    expect(typeof response.getAgentPort()).toBe('number');
+    expect(typeof response.getProxyHost()).toBe('string');
+    expect(typeof response.getProxyPort()).toBe('number');
+    expect(typeof response.getRootPublicKeyPem()).toBe('string');
+    expect(typeof response.getRootCertPem()).toBe('string');
+    expect(typeof response.getRootCertChainPem()).toBe('string');
+    expect(pkClient.grpcClient.secured).toBeTruthy();
   });
 });
