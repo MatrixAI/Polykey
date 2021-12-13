c7eaaa95f0e0c89b62db0a5cb9dc2cc0e07782f0
Roger Qiu <roger.qiu@polyhack.io>
2021-12-13 12:05:18 +1100

    Fixing up `pk agent lock` tests

--- tests/bin/agent/lock.test.ts
+++ tests/bin/agent/lock.test.ts
@@ -2,25 +2,33 @@ import type { SessionToken } from '@/sessions/types';
 import os from 'os';
 import path from 'path';
 import fs from 'fs';
+import prompts from 'prompts';
+import { mocked } from 'ts-jest/utils';
 import Logger, { LogLevel, StreamHandler } from '@matrixai/logger';
 import { Session } from '@/sessions';
 import config from '@/config';
 import * as testBinUtils from '../utils';
 
+/**
+ * Mock prompts module which is used prompt for password
+ */
+jest.mock('prompts');
+const mockedPrompts = mocked(prompts);
+
 describe('lock', () => {
   const logger = new Logger('lock test', LogLevel.WARN, [new StreamHandler()]);
-  let dataDir: string;
-  let pkAgentClose;
   const sessionTokenPath = path.join(
     global.binAgentDir,
     config.defaults.tokenBase,
   );
+  let pkAgentClose;
   beforeAll(async () => {
     pkAgentClose = await testBinUtils.pkAgent();
   }, global.maxTimeout);
   afterAll(async () => {
     await pkAgentClose();
   });
+  let dataDir: string;
   beforeEach(async () => {
     dataDir = await fs.promises.mkdtemp(
       path.join(os.tmpdir(), 'polykey-test-'),
@@ -32,27 +40,56 @@ describe('lock', () => {
       recursive: true,
     });
   });
-  test('should delete the session file', async () => {
-    // Ensure session file exists
+  test('lock deletes the session token', async () => {
     const session = await Session.createSession({
       sessionTokenPath,
       fs,
       logger,
     });
     await session.writeToken('abc' as SessionToken);
-    // Run command
     const { exitCode } = await testBinUtils.pkStdio(
       ['agent', 'lock'],
       {
         PK_NODE_PATH: global.binAgentDir,
-        PK_PASSWORD: global.binAgentPassword,
       },
       global.binAgentDir,
     );
     expect(exitCode).toBe(0);
-    // Check file does not exist
-    await expect(
-      fs.promises.readdir(path.join(global.binAgentDir)),
-    ).resolves.not.toContain('token');
+    expect(await session.readToken()).toBeUndefined();
+  });
+  test('lock ensures reauthentication is required', async () => {
+    const password = global.binAgentPassword;
+    mockedPrompts.mockClear();
+    mockedPrompts.mockImplementation(async (_opts: any) => {
+      return { password };
+    });
+    // Session token is set
+    await testBinUtils.pkStdio(
+      ['agent', 'status'],
+      {
+        PK_NODE_PATH: global.binAgentDir,
+        PK_PASSWORD: global.binAgentPassword,
+      },
+      global.binAgentDir
+    );
+    // Session token is deleted
+    await testBinUtils.pkStdio(
+      ['agent', 'lock'],
+      {
+        PK_NODE_PATH: global.binAgentDir,
+      },
+      global.binAgentDir,
+    );
+    // Will prompt to reauthenticate
+    await testBinUtils.pkStdio(
+      ['agent', 'status'],
+      {
+        PK_NODE_PATH: global.binAgentDir,
+      },
+      global.binAgentDir
+    );
+    // Prompted for password 1 time
+    expect(mockedPrompts.mock.calls.length).toBe(1);
+    mockedPrompts.mockClear();
   });
 });
