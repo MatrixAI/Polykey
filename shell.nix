26dddde9078aec5bbcd02572b07f1c3eb413ffb9
Emma Casolin <emma.casolin@matrix.ai>
2022-05-13 10:18:05 +1000

    build: updated dependencies and added incremental build

--- shell.nix
+++ shell.nix
@@ -21,9 +21,9 @@ in
       set +o allexport
       set -v
 
-      # Enables npm link to work
-      export npm_config_prefix=~/.npm
+      mkdir --parents "$(pwd)/tmp"
 
+      # Built executables and NPM executables
       export PATH="$(pwd)/dist/bin:$(npm bin):$PATH"
 
       # pkg is installed in package.json
@@ -34,8 +34,10 @@ in
         ]
       }:$PATH"
 
+      # Enables npm link to work
+      export npm_config_prefix=~/.npm
+
       npm install
-      mkdir --parents "$(pwd)/tmp"
 
       set +v
     '';
