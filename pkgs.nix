d555a9e674f4383d09a049d84a1fbeb3f8256e12
Roger Qiu <roger.qiu@polyhack.io>
2022-07-10 17:00:22 +1000

    nix: upgrade to 22.05 ce6aa13369b667ac2542593170993504932eb836

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (
-  let rev = "a5774e76bb8c3145eac524be62375c937143b80c"; in
+  let rev = "ce6aa13369b667ac2542593170993504932eb836"; in
   builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz"
 )
