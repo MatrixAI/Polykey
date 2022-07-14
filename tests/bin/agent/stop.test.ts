dee2bd69ef216e0309f6e92fc919131a23c66090
Brian Botha <brian.botha@matrix.ai>
2022-07-14 18:00:10 +1000

    test: converted stop.test.ts
    Related #407

--- tests/bin/agent/stop.test.ts
+++ tests/bin/agent/stop.test.ts
@@ -1,4 +1,3 @@
-import os from 'os';
 import path from 'path';
 import fs from 'fs';
 import Logger, { LogLevel, StreamHandler } from '@matrixai/logger';
@@ -8,13 +7,14 @@ import { sleep } from '@/utils';
 import * as binErrors from '@/bin/errors';
 import * as clientErrors from '@/client/errors';
 import * as testBinUtils from '../utils';
+import { runTestIfPlatforms } from '../../utils';
 
 describe('stop', () => {
   const logger = new Logger('stop test', LogLevel.WARN, [new StreamHandler()]);
   let dataDir: string;
   beforeEach(async () => {
     dataDir = await fs.promises.mkdtemp(
-      path.join(os.tmpdir(), 'polykey-test-'),
+      path.join(global.testDir, 'polykey-test-'),
     );
   });
   afterEach(async () => {
@@ -23,11 +23,11 @@ describe('stop', () => {
       recursive: true,
     });
   });
-  test(
+  runTestIfPlatforms('linux', 'docker')(
     'stop LIVE agent',
     async () => {
       const password = 'abc123';
-      const { exitCode } = await testBinUtils.pkStdio(
+      const { exitCode } = await testBinUtils.pkStdioSwitch(global.testCmd)(
         [
           'agent',
           'start',
@@ -58,7 +58,7 @@ describe('stop', () => {
         fs,
         logger,
       });
-      await testBinUtils.pkStdio(
+      await testBinUtils.pkStdioSwitch(global.testCmd)(
         ['agent', 'stop'],
         {
           PK_NODE_PATH: path.join(dataDir, 'polykey'),
@@ -70,7 +70,7 @@ describe('stop', () => {
     },
     global.defaultTimeout * 2,
   );
-  test(
+  runTestIfPlatforms('linux', 'docker')(
     'stopping is idempotent during concurrent calls and STOPPING or DEAD status',
     async () => {
       const password = 'abc123';
@@ -86,7 +86,7 @@ describe('stop', () => {
         fs,
         logger,
       });
-      const { exitCode } = await testBinUtils.pkStdio(
+      const { exitCode } = await testBinUtils.pkStdioSwitch(global.testCmd)(
         [
           'agent',
           'start',
@@ -110,14 +110,14 @@ describe('stop', () => {
       await status.waitFor('LIVE');
       // Simultaneous calls to stop must use pkExec
       const [agentStop1, agentStop2] = await Promise.all([
-        testBinUtils.pkExec(
+        testBinUtils.pkExecSwitch(global.testCmd)(
           ['agent', 'stop', '--password-file', passwordPath],
           {
             PK_NODE_PATH: path.join(dataDir, 'polykey'),
           },
           dataDir,
         ),
-        testBinUtils.pkExec(
+        testBinUtils.pkExecSwitch(global.testCmd)(
           ['agent', 'stop', '--password-file', passwordPath],
           {
             PK_NODE_PATH: path.join(dataDir, 'polykey'),
@@ -129,7 +129,7 @@ describe('stop', () => {
       // It's not reliable until file watching is implemented
       // So just 1 ms delay until sending another stop command
       await sleep(1);
-      const agentStop3 = await testBinUtils.pkStdio(
+      const agentStop3 = await testBinUtils.pkStdioSwitch(global.testCmd)(
         ['agent', 'stop', '--node-path', path.join(dataDir, 'polykey')],
         {
           PK_PASSWORD: password,
@@ -137,7 +137,7 @@ describe('stop', () => {
         dataDir,
       );
       await status.waitFor('DEAD');
-      const agentStop4 = await testBinUtils.pkStdio(
+      const agentStop4 = await testBinUtils.pkStdioSwitch(global.testCmd)(
         ['agent', 'stop', '--password-file', passwordPath],
         {
           PK_NODE_PATH: path.join(dataDir, 'polykey'),
@@ -159,7 +159,7 @@ describe('stop', () => {
     },
     global.defaultTimeout * 2,
   );
-  test(
+  runTestIfPlatforms('linux', 'docker')(
     'stopping starting agent results in error',
     async () => {
       const password = 'abc123';
@@ -173,7 +173,7 @@ describe('stop', () => {
         fs,
         logger,
       });
-      await testBinUtils.pkSpawn(
+      await testBinUtils.pkSpawnSwitch(global.testCmd)(
         [
           'agent',
           'start',
@@ -196,7 +196,7 @@ describe('stop', () => {
         logger,
       );
       await status.waitFor('STARTING');
-      const { exitCode, stderr } = await testBinUtils.pkStdio(
+      const { exitCode, stderr } = await testBinUtils.pkStdioSwitch(global.testCmd)(
         ['agent', 'stop', '--format', 'json'],
         {
           PK_NODE_PATH: path.join(dataDir, 'polykey'),
@@ -207,7 +207,7 @@ describe('stop', () => {
         new binErrors.ErrorCLIPolykeyAgentStatus('agent is starting'),
       ]);
       await status.waitFor('LIVE');
-      await testBinUtils.pkStdio(
+      await testBinUtils.pkStdioSwitch(global.testCmd)(
         ['agent', 'stop'],
         {
           PK_NODE_PATH: path.join(dataDir, 'polykey'),
@@ -219,11 +219,11 @@ describe('stop', () => {
     },
     global.defaultTimeout * 2,
   );
-  test(
+  runTestIfPlatforms('linux', 'docker')(
     'stopping while unauthenticated does not stop',
     async () => {
       const password = 'abc123';
-      await testBinUtils.pkStdio(
+      await testBinUtils.pkStdioSwitch(global.testCmd)(
         [
           'agent',
           'start',
@@ -253,7 +253,7 @@ describe('stop', () => {
         fs,
         logger,
       });
-      const { exitCode, stderr } = await testBinUtils.pkStdio(
+      const { exitCode, stderr } = await testBinUtils.pkStdioSwitch(global.testCmd)(
         ['agent', 'stop', '--format', 'json'],
         {
           PK_NODE_PATH: path.join(dataDir, 'polykey'),
@@ -269,7 +269,7 @@ describe('stop', () => {
       const statusInfo = await status.readStatus();
       expect(statusInfo).toBeDefined();
       expect(statusInfo?.status).toBe('LIVE');
-      await testBinUtils.pkStdio(
+      await testBinUtils.pkStdioSwitch(global.testCmd)(
         ['agent', 'stop'],
         {
           PK_NODE_PATH: path.join(dataDir, 'polykey'),
