1974f1e0537c7b225169a6de4a01b45881187a44
Roger Qiu <roger.qiu@polyhack.io>
2022-07-20 13:38:37 +1000

    WIP

---
+++ src/queue/Queue.ts
@@ -0,0 +1,26 @@
+// epic queue
+// need to do a couple things:
+// 1. integrate fast-check
+// 2. integrate span checks
+// 3. might also consider span logs?
+// 4. open tracing observability
+// 5. structured logging
+// 6. async hooks to get traced promises to understand the situation
+// 7. do we also get fantasy land promises? and async cancellable stuff?
+// 8. task abstractions?
+// need to use the db for this
+// 9. priority structure
+// 10. timers
+// abort controller
+
+// timeout scheduling system is task scheduling
+// queue is the persistent version of this
+// in general you have a job scheduling problem
+// but lighter weight
+
+class Queue {
+
+
+}
+
+export default Queue;
