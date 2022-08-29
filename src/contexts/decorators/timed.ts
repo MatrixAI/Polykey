fc56ebce5222f6fa2ea05880687dd1f64c736d14
Roger Qiu <roger.qiu@polyhack.io>
2022-08-29 15:18:26 +1000

    WIP: timed decorator

--- src/contexts/decorators/timed.ts
+++ src/contexts/decorators/timed.ts
@@ -1,3 +1,4 @@
+import type { ContextTimed } from '../types';
 import * as contextsUtils from '../utils';
 import * as contextsErrors from '../errors';
 import Timer from '../../timer/Timer';
@@ -7,61 +8,106 @@ import {
   AsyncGeneratorFunction,
 } from '../../utils';
 
-// @timed(100, TimeoutError)
-// do we "construct" the exception
-// or should we pass in the Error()
-// at that point?
-// becuase if you get it in the context
-// the stack looks wrong
-// yea, that makes sense...
-// the problem is that it doesn't show the context of the call
-// ti shows the context of where it was first constructed
-// but on the otherh and, we end up with the same problem
-// and you can provide a "default" error in this case
-// it should be "overridable" then
-// of course the issue is that you have pass a unction can be newed
-// so { new : }
-// and then you don't have any constructor relationship
-// or you have to put it in the right interface
-// at any case... at the very least the stack trace is about who this is called!!!
-// but at the same time time...
-// type X = new (...args: Array<any>) => Error;
-// const x: X = Error;
-// const y = new x('abc');
-// new timeoutError
-// new errorDestroyed
-// throw errorDestroyed
-
-function checkContext(
-  context: any,
+/**
+ * This sets up the context
+ * This will mutate the `params` parameter
+ * It returns a teardown function to be called
+ * when the target function is finished
+ */
+function setupContext(
+  delay: number,
+  errorTimeout: new () => Error,
   targetName: string,
   key: string | symbol,
-): asserts context is
-  | {
-      timer: Timer | undefined;
-      signal: AbortSignal | undefined;
-    }
-  | undefined {
-  if (
-    context !== undefined &&
-    (typeof context !== 'object' || context === null)
-  ) {
+  contextIndex: number,
+  params: Array<any>,
+): () => void {
+  let context: Partial<ContextTimed> = params[contextIndex];
+  if (context === undefined) {
+    context = {}
+    params[contextIndex] = context;
+  }
+  // Runtime type check on the context parameter
+  if (typeof context !== 'object' || context === null) {
     throw new TypeError(
       `\`${targetName}.${key.toString()}\` decorated \`@context\` parameter is not a context object`,
     );
   }
-  if (context?.timer !== undefined && !(context.timer instanceof Timer)) {
+  if (context.timer !== undefined && !(context.timer instanceof Timer)) {
     throw new TypeError(
       `\`${targetName}.${key.toString()}\` decorated \`@context\` parameter's \`timer\` property is not an instance of \`Timer\``,
     );
   }
-  if (
-    context?.signal !== undefined &&
-    !(context.signal instanceof AbortSignal)
-  ) {
+  if (context.signal !== undefined && !(context.signal instanceof AbortSignal)) {
     throw new TypeError(
       `\`${targetName}.${key.toString()}\` decorated \`@context\` parameter's \`signal\` property is not an instance of \`AbortSignal\``,
     );
+  }
+  // Mutating the `context` parameter
+  if (
+    context.timer === undefined &&
+    context.signal === undefined
+  ) {
+    const abortController = new AbortController();
+    const timer = new Timer(
+      () => void abortController.abort(new errorTimeout()),
+      delay,
+    );
+    context.signal = abortController.signal;
+    context.timer = timer;
+    return () => {
+      // Ignore the cancellation
+      void timer.catch(() => {});
+      timer.cancel();
+    };
+  } else if (
+    context.timer === undefined &&
+    context.signal instanceof AbortSignal
+  ) {
+    const abortController = new AbortController();
+    const timer = new Timer(
+      () => void abortController.abort(new errorTimeout()),
+      delay,
+    );
+    // Chain the upstream abort signal to this context
+    const signalHandler = () => void abortController.abort(context.signal!.reason);
+    context.signal.addEventListener('abort', signalHandler);
+    // Overwrite the signal property with this context's `AbortController.signal`
+    context.signal = abortController.signal;
+    context.timer = timer;
+    return () => {
+      context.signal!.removeEventListener('abort', signalHandler);
+      // Ignore the cancellation
+      void timer.catch(() => {});
+      timer.cancel();
+    };
+  } else if (
+    context.timer instanceof Timer &&
+    context.signal === undefined
+  ) {
+    const abortController = new AbortController();
+    let finished = false;
+    // If the timer resolves, then abort the target function
+    context.timer.then(
+      (r: any, s: AbortSignal) => {
+        // If the timer is aborted after it resolves
+        // then don't bother aborting the target function
+        if (!finished || !s.aborted) {
+          abortController.abort(
+            new errorTimeout(),
+          );
+        }
+        return r;
+      }
+    );
+    context.signal = abortController.signal;
+    return () => {
+      finished = true;
+    };
+  } else {
+    // In this case, `context.timer` and `context.signal` are both instances of
+    // `Timer` and `AbortSignal` respectively
+    return () => {};
   }
 }
 
@@ -73,9 +119,10 @@ function timed(
   errorTimeout: new () => Error = contextsErrors.ErrorContextsTimerExpired,
 ) {
   return <
-    T extends (
-      ...params: Array<any>
-    ) => Promise<unknown> | Generator | AsyncGenerator,
+    T extends ((...params: Array<any>) =>
+      Promise<any> |
+      Generator<any, any, any> |
+      AsyncGenerator<any, any, any>)
   >(
     target: any,
     key: string | symbol,
@@ -96,110 +143,68 @@ function timed(
         `\`${targetName}.${key.toString()}\` does not have a \`@context\` parameter decorator`,
       );
     }
-
-    // Const checkContext = (context: any): asserts context is { timer: Timer | undefined; signal: AbortSignal | undefined } | undefined => {
-    // };
-
-    const wrap = (that: any, params: Array<any>) => {
-      const context: unknown = params[contextIndex];
-      checkContext(context, target, key);
-
-      // Now `context: { timer: Timer | undefined; signal: AbortSignal | undefined } | undefined`
-      if (
-        context === undefined ||
-        (context.timer === undefined && context.signal === undefined)
-      ) {
-        const abortController = new AbortController();
-        const timer = new Timer(
-          () => void abortController.abort(new errorTimeout()),
-          delay,
-        );
-        params[contextIndex] = context !== undefined ? context : {};
-        params[contextIndex].signal = abortController.signal;
-        params[contextIndex].timer = timer;
-        const result = f.apply(that, params);
-
-        // Ok an error timeout
-        // it sent in
-        // it may then abort in a number of ways
-        // who knows
-        // we don't know what the result returns
-        // like AT ALL
-        // it only makes sense if this a promise
-        // but now it doesn't even make sense at all here
-        // if you are "cancelling" the timeout
-        // i don't think this was the same
-
-        class ErrorX extends Error {}
-
-        timer.catch((e) => {
-          // Ignore cancellation
-          if (!(e instanceof ErrorX)) {
-            throw e;
-          }
-        });
-        timer.cancel(new ErrorX());
-        return result;
-      } else if (
-        context.timer === undefined &&
-        context.signal instanceof AbortSignal
-      ) {
-        const abortController = new AbortController();
-        const timer = new Timer({
-          delay,
-          handler: () =>
-            void abortController.abort(
-              new contextsErrors.ErrorContextsTimerExpired(),
-            ),
-        });
-        context.signal.onabort = () =>
-          void abortController.abort(context.signal.reason);
-        params[contextIndex].signal = abortController.signal;
-        params[contextIndex].timer = timer;
-        const result = f.apply(that, params);
-        timer.catch((e) => {
-          // Ignore cancellation
-          if (!(e instanceof timerErrors.ErrorTimerCancelled)) {
-            throw e;
-          }
-        });
-        timer.cancel();
-        return result;
-      } else if (
-        context.timer instanceof Timer &&
-        context.signal === undefined
-      ) {
-        const abortController = new AbortController();
-        context.timer.then(
-          () =>
-            void abortController.abort(
-              new contextsErrors.ErrorContextsTimerExpired(),
-            ),
-        );
-        params[contextIndex].signal = abortController.signal;
-        return f.apply(that, params);
-      } else if (
-        context.timer instanceof Timer &&
-        context.signal instanceof AbortSignal
-      ) {
-        return f.apply(that, params);
-      }
-    };
     if (f instanceof AsyncFunction) {
       descriptor['value'] = async function (...params) {
-        return wrap(this, params);
+        const teardownContext = setupContext(
+          delay,
+          errorTimeout,
+          target,
+          key,
+          contextIndex,
+          params,
+        );
+        try {
+          const result = await f.apply(this, params);
+          return result;
+        } finally {
+          teardownContext();
+        }
       };
     } else if (f instanceof GeneratorFunction) {
       descriptor['value'] = function* (...params) {
-        return yield* wrap(this, params);
+        const teardownContext = setupContext(
+          delay,
+          errorTimeout,
+          target,
+          key,
+          contextIndex,
+          params,
+        );
+        try {
+          return yield* f.apply(this, params);
+        } finally {
+          teardownContext();
+        }
       };
     } else if (f instanceof AsyncGeneratorFunction) {
       descriptor['value'] = async function* (...params) {
-        return yield* wrap(this, params);
+        const teardownContext = setupContext(
+          delay,
+          errorTimeout,
+          target,
+          key,
+          contextIndex,
+          params,
+        );
+        try {
+          return yield* f.apply(this, params);
+        } finally {
+          teardownContext();
+        }
       };
     } else {
       descriptor['value'] = function (...params) {
-        return wrap(this, params);
+        const teardownContext = setupContext(
+          delay,
+          errorTimeout,
+          target,
+          key,
+          contextIndex,
+          params,
+        );
+        return f.apply(this, params).finally(() => {
+          teardownContext();
+        });
       };
     }
     // Preserve the name
