f88b157e5718fef38e7d16996690e35675b70c6f
Roger Qiu <roger.qiu@polyhack.io>
2022-09-06 01:35:37 +1000

    WIP: cancellable decorator and tests

--- tests/contexts/decorators/cancellable.test.ts
+++ tests/contexts/decorators/cancellable.test.ts
@@ -1,6 +1,8 @@
 import type { ContextCancellable } from '@/contexts/types';
+import { PromiseCancellable } from '@matrixai/async-cancellable';
 import context from '@/contexts/decorators/context';
 import cancellable from '@/contexts/decorators/cancellable';
+import { AsyncFunction } from '@/utils';
 
 describe('context/decorators/cancellable', () => {
   describe('cancellable decorator runtime validation', () => {
@@ -27,6 +29,67 @@ describe('context/decorators/cancellable', () => {
         // @ts-ignore invalid context signal
         await c.f({ signal: 'lol' });
       }).rejects.toThrow(TypeError);
+    });
+  });
+  describe('cancellable decorator syntax', () => {
+    // Decorators cannot change type signatures
+    // use overloading to change required context parameter to optional context parameter
+    const symbolFunction = Symbol('sym');
+    class X {
+      functionPromise(
+        ctx?: Partial<ContextCancellable>,
+      ): PromiseCancellable<void>;
+      @cancellable()
+      functionPromise(
+        @context ctx: ContextCancellable,
+      ): Promise<void> {
+        expect(ctx.signal).toBeInstanceOf(AbortSignal);
+        return new Promise((resolve) => void resolve());
+      }
+
+      asyncFunction(
+        ctx?: Partial<ContextCancellable>,
+      ): PromiseCancellable<void>;
+      @cancellable(true)
+      async asyncFunction(
+        @context ctx: ContextCancellable,
+      ): Promise<void> {
+        expect(ctx.signal).toBeInstanceOf(AbortSignal);
+      }
+
+      [symbolFunction](
+        ctx?: Partial<ContextCancellable>,
+      ): PromiseCancellable<void>;
+      @cancellable(false)
+      [symbolFunction](
+        @context ctx: ContextCancellable,
+      ): Promise<void> {
+        expect(ctx.signal).toBeInstanceOf(AbortSignal);
+        return new Promise((resolve) => void resolve());
+      }
+    }
+    const x = new X();
+    test('functionPromise', async () => {
+      await x.functionPromise();
+      await x.functionPromise({});
+      await x.functionPromise({ signal: (new AbortController()).signal });
+      expect(x.functionPromise).toBeInstanceOf(Function);
+      expect(x.functionPromise.name).toBe('functionPromise');
+    });
+    test('asyncFunction', async () => {
+      await x.asyncFunction();
+      await x.asyncFunction({});
+      await x.asyncFunction({ signal: (new AbortController()).signal });
+      expect(x.asyncFunction).toBeInstanceOf(Function);
+      expect(x.asyncFunction).not.toBeInstanceOf(AsyncFunction);
+      expect(x.asyncFunction.name).toBe('asyncFunction');
+    });
+    test('symbolFunction', async () => {
+      await x[symbolFunction]();
+      await x[symbolFunction]({});
+      await x[symbolFunction]({ signal: (new AbortController()).signal });
+      expect(x[symbolFunction]).toBeInstanceOf(Function);
+      expect(x[symbolFunction].name).toBe('[sym]');
     });
   });
 });
