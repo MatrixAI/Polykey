25eefe78bd105d0bddbcb786bdfc8099d5fbadbc
Roger Qiu <roger.qiu@polyhack.io>
2022-09-07 13:13:29 +1000

    WIP: tasks

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -1,4 +1,4 @@
-export { default as Scheduler } from './Scheduler';
+export { default as Tasks } from './Tasks';
 export * as types from './types';
 export * as utils from './utils';
 export * as errors from './errors';
