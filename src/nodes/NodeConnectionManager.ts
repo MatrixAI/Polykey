f11197c24ac8e5799284f7dcb7c47eaa8f2d6deb
Brian Botha <brian.botha@matrix.ai>
2022-09-21 15:16:18 +1000

    fix: updated default timeout for `NodeConnectionManager.pingNode`

--- src/nodes/NodeConnectionManager.ts
+++ src/nodes/NodeConnectionManager.ts
@@ -722,7 +722,7 @@ class NodeConnectionManager {
     ctx?: Partial<ContextTimed>,
   ): PromiseCancellable<boolean>;
   @ready(new nodesErrors.ErrorNodeConnectionManagerNotRunning())
-  @timedCancellable(true, 20000)
+  @timedCancellable(true, 2000)
   public async pingNode(
     nodeId: NodeId,
     host: Host | Hostname,
