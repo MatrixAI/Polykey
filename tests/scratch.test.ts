18cd6bbd7d1fc76f165b179891da2c10474ab124
Brian Botha <brian.botha@matrix.ai>
2022-09-23 18:18:31 +1000

    ci: adding a scratch job and test file
    This allows us to run a specific test quickly via the CI during the first stage. Should allow for quicker testing.

---
+++ tests/scratch.test.ts
@@ -0,0 +1,47 @@
+import type { DB } from '@matrixai/db';
+import type TaskManager from '@/tasks/TaskManager';
+import type KeyManager from '@/keys/KeyManager';
+import type NodeConnectionManager from '@/nodes/NodeConnectionManager';
+import type NodeGraph from '@/nodes/NodeGraph';
+import type Sigchain from '@/sigchain/Sigchain';
+import Logger, { LogLevel, StreamHandler } from '@matrixai/logger';
+import NodeManager from '@/nodes/NodeManager';
+
+// This is a 'scratch paper' test file for quickly running tests in the CI
+describe('scratch', () => {
+  const logger = new Logger(`${NodeManager.name} test`, LogLevel.INFO, [
+    new StreamHandler(),
+  ]);
+
+  test('Should have unique HandlerIds', async () => {
+    const nodeManager = new NodeManager({
+      db: {} as DB,
+      sigchain: {} as Sigchain,
+      keyManager: {} as KeyManager,
+      nodeGraph: {} as NodeGraph,
+      nodeConnectionManager: {} as NodeConnectionManager,
+      taskManager: {} as TaskManager,
+      logger,
+    });
+    logger.info('checking names');
+    logger.info(nodeManager.basePath);
+    logger.info(nodeManager.refreshBucketHandlerId);
+    logger.info(nodeManager.gcBucketHandlerId);
+    logger.info(nodeManager.refreshBucketHandlerId);
+    logger.info('end of names');
+    expect(nodeManager.gcBucketHandlerId).not.toEqual(
+      nodeManager.refreshBucketHandlerId,
+    );
+    expect(nodeManager.gcBucketHandlerId).not.toEqual(
+      nodeManager.pingAndSetNodeHandlerId,
+    );
+    expect(nodeManager.refreshBucketHandlerId).not.toEqual(
+      nodeManager.pingAndSetNodeHandlerId,
+    );
+  });
+});
+
+// We can't have empty test files so here is a sanity test
+test('Should avoid empty test suite', async () => {
+  expect(1 + 1).toBe(2);
+});
