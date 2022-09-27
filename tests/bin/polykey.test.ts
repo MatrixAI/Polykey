cf4d76314a1ade6f8bbe2f60eec6271cbb0e98b6
Roger Qiu <roger.qiu@polyhack.io>
2022-09-27 16:03:33 +1000

    test: removed `os` from `tests/bin/polykey.test.ts` as is no longer used

--- tests/bin/polykey.test.ts
+++ tests/bin/polykey.test.ts
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-import os from 'os';
 import readline from 'readline';
 import Logger, { LogLevel, StreamHandler } from '@matrixai/logger';
 import * as testUtils from '../utils';
