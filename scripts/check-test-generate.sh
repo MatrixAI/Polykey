c1459582a99321536c06e051867207520ebb0110
Brian Botha <brian.botha@matrix.ai>
2022-09-28 12:16:29 +1000

    tests: disabling discovery tests in `check:test-generate`

--- scripts/check-test-generate.sh
+++ scripts/check-test-generate.sh
@@ -48,6 +48,10 @@ printf "\n"
 
 # Each test directory has its own job
 for test_dir in tests/**/*/; do
+  # Ignore discovery domain for now
+  if [[ "$test_dir" =~ discovery ]]; then
+    continue
+  fi
   test_files=("$test_dir"*.test.ts)
   if [ ${#test_files[@]} -eq 0 ]; then
     continue
