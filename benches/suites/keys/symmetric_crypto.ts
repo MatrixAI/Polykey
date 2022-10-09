3f186bf625039b4d980227d0481f8c0820757869
Roger Qiu <roger.qiu@polyhack.io>
2022-10-09 13:44:24 +1100

    WIP: benching after libsodium [ci skip]

--- benches/suites/keys/symmetric_crypto.ts
+++ benches/suites/keys/symmetric_crypto.ts
@@ -5,32 +5,32 @@ import * as symmetric from '@/keys/utils/symmetric';
 import { summaryName, suiteCommon } from '../../utils';
 
 async function main() {
-  const key = await generate.generateKey(256);
-  const plain512B = random.getRandomBytesSync(512);
-  const plain1KiB = random.getRandomBytesSync(1024);
-  const plain10KiB = random.getRandomBytesSync(1024 * 10);
-  const cipher512B = await symmetric.encryptWithKey(key, plain512B);
-  const cipher1KiB = await symmetric.encryptWithKey(key, plain1KiB);
-  const cipher10KiB = await symmetric.encryptWithKey(key, plain10KiB);
+  const key = generate.generateKey();
+  const plain512B = random.getRandomBytes(512);
+  const plain1KiB = random.getRandomBytes(1024);
+  const plain10KiB = random.getRandomBytes(1024 * 10);
+  const cipher512B = symmetric.encryptWithKey(key, plain512B);
+  const cipher1KiB = symmetric.encryptWithKey(key, plain1KiB);
+  const cipher10KiB = symmetric.encryptWithKey(key, plain10KiB);
   const summary = await b.suite(
     summaryName(__filename),
-    b.add('encrypt 512 B of data', async () => {
-      await symmetric.encryptWithKey(key, plain512B);
+    b.add('encrypt 512 B of data', () => {
+      symmetric.encryptWithKey(key, plain512B);
     }),
-    b.add('encrypt 1 KiB of data', async () => {
-      await symmetric.encryptWithKey(key, plain1KiB);
+    b.add('encrypt 1 KiB of data', () => {
+      symmetric.encryptWithKey(key, plain1KiB);
     }),
-    b.add('encrypt 10 KiB of data', async () => {
-      await symmetric.encryptWithKey(key, plain10KiB);
+    b.add('encrypt 10 KiB of data', () => {
+      symmetric.encryptWithKey(key, plain10KiB);
     }),
-    b.add('decrypt 512 B of data', async () => {
-      await symmetric.decryptWithKey(key, cipher512B);
+    b.add('decrypt 512 B of data', () => {
+      symmetric.decryptWithKey(key, cipher512B);
     }),
-    b.add('decrypt 1 KiB of data', async () => {
-      await symmetric.decryptWithKey(key, cipher1KiB);
+    b.add('decrypt 1 KiB of data', () => {
+      symmetric.decryptWithKey(key, cipher1KiB);
     }),
-    b.add('decrypt 10 KiB of data', async () => {
-      await symmetric.decryptWithKey(key, cipher10KiB);
+    b.add('decrypt 10 KiB of data', () => {
+      symmetric.decryptWithKey(key, cipher10KiB);
     }),
     ...suiteCommon,
   );
