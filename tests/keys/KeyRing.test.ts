ccc7f7f5eae0c1c4561021ed2d09fd9622c5fd13
Roger Qiu <roger.qiu@polyhack.io>
2022-10-09 22:25:15 +1100

    WIP KeyRing testing [ci skip]

--- tests/keys/KeyRing.test.ts
+++ tests/keys/KeyRing.test.ts
@@ -1,12 +1,15 @@
 import fs from 'fs';
 import os from 'os';
 import path from 'path';
+import { testProp, fc } from '@fast-check/jest';
 import Logger, { LogLevel, StreamHandler } from '@matrixai/logger';
-import KeyRing from '../../src/keys/KeyRing';
+import KeyRing from '@/keys/KeyRing';
+import * as keysUtils from '@/keys/utils';
 import * as keysErrors from '@/keys/errors';
+import * as testsKeysUtils from './utils';
 
 describe(KeyRing.name, () => {
-  const password = 'password';
+  const password = keysUtils.getRandomBytes(10).toString('utf-8');
   const logger = new Logger(`${KeyRing.name} Test`, LogLevel.WARN, [
     new StreamHandler(),
   ]);
@@ -22,6 +25,12 @@ describe(KeyRing.name, () => {
       recursive: true,
     });
   });
+  // testProp(
+  //   'KeyRing readiness',
+  //   [ testsKeysUtils.passwordArb, ],
+  //   async (password) => {
+  //   }
+  // );
   test('KeyRing readiness', async () => {
     const keysPath = `${dataDir}/keys`;
     const keyRing = await KeyRing.createKeyRing({
@@ -45,5 +54,24 @@ describe(KeyRing.name, () => {
     await expect(async () => {
       await keyRing.checkPassword(password);
     }).rejects.toThrow(keysErrors.ErrorKeyRingNotRunning);
+  });
+  test('constructs root key pair, and db key', async () => {
+    const keysPath = `${dataDir}/keys`;
+    const keyRing = await KeyRing.createKeyRing({
+      keysPath,
+      password,
+      logger,
+    });
+    const keysPathContents = await fs.promises.readdir(keysPath);
+    expect(keysPathContents).toContain('public.jwk');
+    expect(keysPathContents).toContain('private.jwk');
+    expect(keysPathContents).toContain('db.jwk');
+    expect(keyRing.keyPair.publicKey).toBeInstanceOf(Buffer);
+    expect(keyRing.keyPair.publicKey.byteLength).toBe(32);
+    expect(keyRing.keyPair.privateKey).toBeInstanceOf(Buffer);
+    expect(keyRing.keyPair.privateKey.byteLength).toBe(32);
+    expect(keyRing.dbKey).toBeInstanceOf(Buffer);
+    expect(keyRing.dbKey.byteLength).toBe(32);
+    await keyRing.stop();
   });
 });
