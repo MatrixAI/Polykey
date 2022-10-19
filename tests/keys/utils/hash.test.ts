cf9a45d95ff115e6d68706b966dbe40525fb1cfb
Roger Qiu <roger.qiu@polyhack.io>
2022-10-20 02:18:49 +1100

    WIP: hashing [ci skip]

--- tests/keys/utils/hash.test.ts
+++ tests/keys/utils/hash.test.ts
@@ -1,5 +1,6 @@
 import { testProp, fc } from '@fast-check/jest';
 import * as hash from '@/keys/utils/hash';
+import * as utils from '@/utils';
 
 describe('keys/utils/hash', () => {
   testProp(
@@ -231,6 +232,28 @@ describe('keys/utils/hash', () => {
       expect(resultSHA2512.value).toStrictEqual(hash.sha2512(Buffer.concat(datas)));
       expect(resultSHA2512256.value).toStrictEqual(hash.sha2512256(Buffer.concat(datas)));
       expect(resultBLAKE2b256.value).toStrictEqual(hash.blake2b256(Buffer.concat(datas)));
+    }
+  );
+  testProp(
+    'to and from multidigest',
+    [fc.uint8Array({ minLength: 0, maxLength: 1024 })],
+    (data) => {
+      const digestSHA2256 = hash.hash(data, 'sha2-256');
+      const digestSHA2512 = hash.hash(data, 'sha2-512');
+      const digestSHA2512256 = hash.hash(data, 'sha2-512-256');
+      const digestBLAKE2b256 = hash.hash(data, 'blake2b-256');
+      const mDigestSHA2256 = hash.digestToMultidigest(digestSHA2256, 'sha2-256');
+      const mDigestSHA2512 = hash.digestToMultidigest(digestSHA2512, 'sha2-512');
+      const mDigestSHA2512256 = hash.digestToMultidigest(digestSHA2512256, 'sha2-512-256');
+      const mDigestBLAKE2b256 =  hash.digestToMultidigest(digestBLAKE2b256, 'blake2b-256');
+      const digestSHA2256_ = hash.digestFromMultidigest(mDigestSHA2256.bytes)!.digest
+      const digestSHA2512_ = hash.digestFromMultidigest(mDigestSHA2512.bytes)!.digest
+      const digestSHA2512256_ = hash.digestFromMultidigest(mDigestSHA2512256.bytes)!.digest
+      const digestBLAKE2b256_ = hash.digestFromMultidigest(mDigestBLAKE2b256.bytes)!.digest
+      expect(utils.bufferWrap(digestSHA2256_)).toStrictEqual(digestSHA2256);
+      expect(utils.bufferWrap(digestSHA2512_)).toStrictEqual(digestSHA2512);
+      expect(utils.bufferWrap(digestSHA2512256_)).toStrictEqual(digestSHA2512256);
+      expect(utils.bufferWrap(digestBLAKE2b256_)).toStrictEqual(digestBLAKE2b256);
     }
   );
 });
