51714f76e8a44fa9161cfaa9f4417cd8fb79b277
Roger Qiu <roger.qiu@polyhack.io>
2022-10-26 22:28:29 +1100

    WIP: removed unused types in nodes domain
    [ci skip]

--- src/nodes/types.ts
+++ src/nodes/types.ts
@@ -1,7 +1,5 @@
 import type { NodeId, NodeIdString, NodeIdEncoded } from '../ids/types';
 import type { Host, Hostname, Port } from '../network/types';
-import type { Claim, ClaimId } from '../claims/types';
-import type { ChainData } from '../sigchain/types';
 
 /**
  * Key indicating which space the NodeGraph is in
@@ -28,53 +26,15 @@ type NodeData = {
 
 type SeedNodes = Record<NodeIdEncoded, NodeAddress>;
 
-/**
- * A claim made on a node. That is, can be either:
- *   - a claim from a node -> node
- *   - a claim from a node -> identity
- * Contains the leveldb key of the claim on the sigchain (this is the
- * lexicographic-integer representation of the claim's sequence number).
- */
-type NodeClaim = Claim & {
-  id: ClaimId;
-};
-
-/**
- * Data structure containing the sigchain data of some node.
- * chain: maps ClaimId (lexicographic integer of sequence number) -> Claim
- */
-type NodeInfo = {
-  id: NodeIdEncoded;
-  chain: ChainData;
-};
-
-// Only 1 domain, so don't need a 'domain' value (like /gestalts/types.ts)
-type NodeGraphOp_ = {
-  // Bucket index
-  key: number;
-  value: NodeBucket;
-};
-
-type NodeGraphOp =
-  | ({
-      type: 'put';
-    } & NodeGraphOp_)
-  | ({
-      type: 'del';
-    } & Omit<NodeGraphOp_, 'value'>);
-
 export type {
   NodeId,
   NodeIdString,
   NodeIdEncoded,
   NodeAddress,
   SeedNodes,
-  NodeClaim,
-  NodeInfo,
   NodeBucketIndex,
   NodeBucketMeta,
   NodeBucket,
   NodeData,
-  NodeGraphOp,
   NodeGraphSpace,
 };
