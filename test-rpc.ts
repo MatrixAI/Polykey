aefbeb5819cdc28b8f44cb6ca323ccf866863c6c
Roger Qiu <roger.qiu@polyhack.io>
2022-11-15 19:03:30 +1100

    WIP [ci skip]

--- test-rpc.ts
+++ test-rpc.ts
@@ -1,18 +1,44 @@
-// we are going to try to use deepkit rpc to figure out if it works for us
-// but we are unlikely to use the library itself because it uses a different ecosystem from us
-// it's so much dependencies
-// you end up needing isomorphic-ws too
-// to get websockets available in nodejs
-
 import { rpc, RpcKernel } from '@deepkit/rpc';
 import { RpcWebSocketServer } from '@deepkit/rpc-tcp';
 
-@rpc.controller('myController');
-class Controller {
+@rpc.controller('Con')
+class Con {
     @rpc.action()
     hello(title: string): string {
         return 'Hello ' + title;
     }
+
+    @rpc.action()
+    async getUser(): Promise<string> {
+      return 'this is a user';
+    }
 }
 
-// instresting
+async function main () {
+
+  const kernel = new RpcKernel();
+  kernel.registerController(Con, 'Con');
+  kernel.controllers
+  kernel.createConnection
+  kernel.onConnection((conn) => {
+    conn.clientAddress
+    conn.controller
+    conn.handleMessage
+    conn.myPeerId
+    conn.onClose
+    conn.onMessage
+    conn.writer
+  });
+
+  const server = new RpcWebSocketServer(kernel, 'ws://localhost:8081');
+
+  server.start({
+    host: 'localhost',
+    port: 8081,
+  });
+
+  console.log('STARTED');
+  // server.close();
+}
+
+main();
