75df9179828edb7262d0e848663cf06467f5876d
Roger Qiu <roger.qiu@polyhack.io>
2023-08-11 15:59:06 +1000

    fix: migrated `utils.nix` to Polykey-CLI

--- shell.nix
+++ shell.nix
@@ -1,40 +1,35 @@
 { pkgs ? import ./pkgs.nix {}, ci ? false }:
 
 with pkgs;
-let
-  utils = callPackage ./utils.nix {};
-in
-  mkShell {
-    nativeBuildInputs = [
-      nodejs
-      shellcheck
-      gitAndTools.gh
-      jq
-    ];
-    PKG_CACHE_PATH = utils.pkgCachePath;
-    PKG_IGNORE_TAG = 1;
-    shellHook = ''
-      echo "Entering $(npm pkg get name)"
-      set -o allexport
-      . ./.env
-      set +o allexport
-      set -v
-      ${
-        lib.optionalString ci
-        ''
-        set -o errexit
-        set -o nounset
-        set -o pipefail
-        shopt -s inherit_errexit
-        ''
-      }
-      mkdir --parents "$(pwd)/tmp"
+mkShell {
+  nativeBuildInputs = [
+    nodejs
+    shellcheck
+    gitAndTools.gh
+    jq
+  ];
+  shellHook = ''
+    echo "Entering $(npm pkg get name)"
+    set -o allexport
+    . ./.env
+    set +o allexport
+    set -v
+    ${
+      lib.optionalString ci
+      ''
+      set -o errexit
+      set -o nounset
+      set -o pipefail
+      shopt -s inherit_errexit
+      ''
+    }
+    mkdir --parents "$(pwd)/tmp"
 
-      # Built executables and NPM executables
-      export PATH="$(pwd)/dist/bin:$(npm root)/.bin:$PATH"
+    # Built executables and NPM executables
+    export PATH="$(pwd)/dist/bin:$(npm root)/.bin:$PATH"
 
-      npm install --ignore-scripts
+    npm install --ignore-scripts
 
-      set +v
-    '';
-  }
+    set +v
+  '';
+}
