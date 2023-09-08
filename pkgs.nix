117fd366cf8b8f54c8b2704acf206f403195db02
Brian Botha <brian.botha@matrix.ai>
2023-09-08 14:58:06 +1000

    wip: node and dependencies versions
    [ci skip]

--- pkgs.nix
+++ pkgs.nix
@@ -1,4 +1,4 @@
 import (
-  let rev = "f294325aed382b66c7a188482101b0f336d1d7db"; in
+  let rev = "ea5234e7073d5f44728c499192544a84244bf35a"; in
   builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz"
 )
