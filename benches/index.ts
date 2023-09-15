8dd41f72ad377c90a6c2dab2b1371f9eb39a461d
Roger Qiu <roger.qiu@polyhack.io>
2023-09-16 00:47:01 +1000

    bench: no longer executes files that do not export a main function, and also force removes the metrics file if it does not exist

--- benches/index.ts
+++ benches/index.ts
@@ -16,11 +16,15 @@ async function main(): Promise<void> {
       continue;
     }
     const suite: () => Promise<Summary> = (await import(suitePath)).default;
-    await suite();
+    // Skip default exports that are not functions and are not called "main"
+    // They might be utility files
+    if (typeof suite === 'function' && suite.name === 'main') {
+      await suite();
+    }
   }
   // Concatenating metrics
   const metricsPath = path.join(resultsPath, 'metrics.txt');
-  await fs.promises.rm(metricsPath);
+  await fs.promises.rm(metricsPath, { force: true });
   let concatenating = false;
   for await (const metricPath of fsWalk(resultsPath)) {
     // Skip over non-metrics files
