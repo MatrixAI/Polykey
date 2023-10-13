6265e3f7a8cbc9ef82ec700020265bd70f5aa8c9
Aditya <38064122+bettercallav@users.noreply.github.com>
2023-10-13 19:51:54 +1100

    fix: increase github authentication timeout to 1000000 from 120000

--- src/identities/providers/github/GitHubProvider.ts
+++ src/identities/providers/github/GitHubProvider.ts
@@ -42,7 +42,7 @@ class GitHubProvider extends Provider {
   }
 
   public async *authenticate(
-    timeout: number = 120000,
+    timeout: number = 1000000,
   ): AsyncGenerator<ProviderAuthenticateRequest, IdentityId> {
     const params = new URLSearchParams();
     params.set('client_id', this.clientId);
