476eeb1c70850e1f7844a952b7178771e743e7d6
Brian Botha <brian.botha@matrix.ai>
2024-04-17 17:27:35 +1000

    feat: added cancellation to `GestaltDiscoveryQueue`
    [ci skip]

--- src/client/handlers/GestaltsDiscoveryQueue.ts
+++ src/client/handlers/GestaltsDiscoveryQueue.ts
@@ -1,6 +1,7 @@
 import type { ClientRPCRequestParams, ClientRPCResponseResult } from '../types';
 import type Discovery from '../../discovery/Discovery';
 import type { DiscoveryQueueInfo } from '../../discovery/types';
+import type { ContextTimed } from '@matrixai/contexts';
 import { ServerHandler } from '@matrixai/rpc';
 
 class GestaltsDiscoveryQueue extends ServerHandler<
@@ -10,11 +11,15 @@ class GestaltsDiscoveryQueue extends ServerHandler<
   ClientRPCRequestParams,
   ClientRPCResponseResult<DiscoveryQueueInfo>
 > {
-  public async *handle(): AsyncGenerator<
-    ClientRPCResponseResult<DiscoveryQueueInfo>
-  > {
+  public async *handle(
+    _input: ClientRPCRequestParams,
+    _cancel,
+    _meta,
+    ctx: ContextTimed,
+  ): AsyncGenerator<ClientRPCResponseResult<DiscoveryQueueInfo>> {
     const { discovery } = this.container;
     for await (const discoveryQueueInfo of discovery.getDiscoveryQueue()) {
+      ctx.signal.throwIfAborted();
       yield discoveryQueueInfo;
     }
   }
