fe3f746d54c279780ef19244746a71fc380c10ef
Brian Botha <brian.botha@matrix.ai>
2024-05-10 17:11:51 +1000

    wip: added benchmark for `listReferencesGenerator`
    [ci skip]

--- benches/index.ts
+++ benches/index.ts
@@ -12,7 +12,10 @@ async function main(): Promise<void> {
   for await (const suitePath of fsWalk(suitesPath)) {
     // Skip over non-ts and non-js files
     const ext = path.extname(suitePath);
-    if (ext !== '.ts' && ext !== '.js') {
+    if (
+      (ext !== '.ts' && ext !== '.js') ||
+      path.basename(suitePath) !== 'git_utils_fs.ts'
+    ) {
       continue;
     }
     const suite: () => Promise<Summary> = (await import(suitePath)).default;
