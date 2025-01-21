1a6a0565a8956846291599b84bdff28f5259c079
Brian Botha <brian.botha@matrix.ai>
2025-01-21 11:06:12 +1100

    fix: reverted `nodesConnectionInitialMaxStreamsBidi` back to `100` to alleviate memory issues in seed nodes

--- src/config.ts
+++ src/config.ts
@@ -251,7 +251,7 @@ const config = {
      * Interval for hole punching reverse node connections.
      */
     nodesConnectionHolePunchIntervalTime: 1_000, // 1 second
-    nodesConnectionInitialMaxStreamsBidi: 200,
+    nodesConnectionInitialMaxStreamsBidi: 100,
     nodesConnectionInitialMaxStreamsUni: 0, // We don't use unidirectional streams so we disable them
 
     /**
