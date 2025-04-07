7b8cc5e1e433442e9cbf9b36fa86d140817c9e3e
Brian Botha <brian.botha@matrix.ai>
2025-04-07 14:55:03 +1000

    fix: fixed importing `.json` with type assertion

--- src/status/utils.ts
+++ src/status/utils.ts
@@ -1,7 +1,7 @@
 import type { JSONSchemaType, ValidateFunction } from 'ajv';
 import type { StatusInfo } from './types.js';
 import Ajv from 'ajv';
-import StatusSchema from './StatusSchema.json';
+import StatusSchema from './StatusSchema.json' assert { type: 'json' };
 
 // @ts-ignore Ajv is improperly exported for ESM
 const ajv = new Ajv.default();
--- src/tokens/schemas/index.ts
+++ src/tokens/schemas/index.ts
@@ -1,7 +1,7 @@
 import type { ValidateFunction } from 'ajv';
 import type { SignedTokenEncoded } from '../types.js';
 import Ajv from 'ajv';
-import SignedTokenEncodedSchema from './SignedTokenEncodedSchema.json';
+import SignedTokenEncodedSchema from './SignedTokenEncodedSchema.json' assert { type: 'json' };
 
 // @ts-ignore: Ajv exports is function improperly for ESM
 const ajv = new Ajv.default();
